48749a776a57feff62598ad8c5be2e61b7fee0b8
Lionel Chong <chonglionel97@gmail.com>
2023-11-04 13:21:06 +0800

    Add in another view

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
+def january(request):
     return HttpResponse("This works!")
+
+
+def february(request):
+    return HttpResponse("Walk for at least 20 minutes each day!")
