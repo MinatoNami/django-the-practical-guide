2375e14602e6acb538f18f9cabdd83fd5ddcf008
Lionel Chong <chonglionel97@gmail.com>
2023-11-04 13:28:36 +0800

    Add dynamic segments

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("This works!")
-
-
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes each day!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "This works!"
+    elif month == "february":
+        challenge_text = "Walk for at least 20 minutes each day!"
+    elif month == "march":
+        challenge_text = "Learn Django for at least 20 minutes each day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
