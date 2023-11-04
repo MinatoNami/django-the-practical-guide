8b18d3361e9097366291f6b667015fc6fe3a74e8
Lionel Chong <chonglionel97@gmail.com>
2023-11-04 14:30:30 +0800

    Add in variable interpolation

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -44,6 +44,9 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "month": month,
+            "text": challenge_text,
+        })
     except:
         return HttpResponseNotFound("This month is not supported!")
